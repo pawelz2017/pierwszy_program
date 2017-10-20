c93901c5ad54e034742dfa5afdb0d6c4c3a3e0be
pawelz2017 <pawel-olsztyn@o2.pl>
2017-10-20 15:35:17 +0200

    kalkutator 1.1

--- program3.c
+++ program3.c
@@ -5,7 +5,7 @@ int mnoz(int a, int b){
 }
 
 double dziel (int a, int b){
-	printf("Wynik dzielenia: %i\n", a / b);
+	printf("Wynik dzielenia: %i i reszta: %i\n", a / b, a % b);
 }
 
 int dodaj(int a, int b){
@@ -16,39 +16,51 @@ int odejmowanie(int a, int b){
 	printf("Wynik odejmowania: %i\n", a - b);
 }
 
+int licz(int a, int b, char wybor){
+	if(wybor == '+') dodaj(a,b);
+	else	if(wybor == '-') odejmowanie(a,b);
+	else	if(wybor == '*') mnoz(a,b);
+	else	if(wybor == '/') dziel(a,b);
+	else	printf("Dzialanie nie obslugiwane\n");
+	return 1;
+}
+
 int main()
 {
 
 	int a;
 	int b;
 	char wybor;
+	int czy_dalej = 1;
+
+	while(czy_dalej == 1){
+
+
 /*
-	printf("Podaj symbol dzialania matematycznego (+, -, *, /)\n");
-	scanf("%c", &wybor);	/*Odczytaj z konsoli i zapisz pod wybor*/
+		printf("Podaj symbol dzialania matematycznego (+, -, *, /)\n");
+		scanf("%c", &wybor);	/*Odczytaj z konsoli i zapisz pod wybor*/
 
 
-	printf("Podaj pierwsza liczbe\n");
-	scanf("%i", &a);	/*Odczytaj z konsoli i zapisz pod wybor*/
+		printf("Podaj pierwsza liczbe\n");
+		scanf("%i", &a);	/*Odczytaj z konsoli i zapisz pod wybor*/
 
+		printf("Podaj symbol dzialania matematycznego (+, -, *, /)\n");
+		scanf(" %c", &wybor);	/*Odczytaj z konsoli i zapisz pod wybor*/
 
-	printf("Podaj druga liczbe\n");
-	scanf("%i", &b);	/*Odczytaj z konsoli i zapisz pod wybor*/
-	
-	
-	printf("Podaj symbol dzialania matematycznego (+, -, *, /)\n");
-	scanf(" %c", &wybor);	/*Odczytaj z konsoli i zapisz pod wybor*/
+		printf("Podaj druga liczbe\n");
+		scanf("%i", &b);	/*Odczytaj z konsoli i zapisz pod wybor*/
+		
+		licz(a, b, wybor);
 	
 /*	
-	printf("debug %i\n", a);
-	printf("debug %i\n", b);
-	printf("debug %c\n", wybor);
+		printf("debug %i\n", a);
+		printf("debug %i\n", b);
+		printf("debug %c\n", wybor);
 */
-	if(wybor == '+') dodaj(a,b);
-	else	if(wybor == '-') odejmowanie(a,b);
-	else	if(wybor == '*') mnoz(a,b);
-	else	if(wybor == '/') dziel(a,b);
-	else	printf("Dzialanie nie obslugiwane\n");
 
+		printf("Czy chcesz dalej liczyc? (1 - tak / 0 - nie: ");
+		scanf("%i", &czy_dalej);
+	}
 
 	return 0;
 }
