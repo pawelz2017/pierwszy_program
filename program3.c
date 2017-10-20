94e390de85c9d06018c3e987b19af07d4e980fe3
pawelz2017 <pawel-olsztyn@o2.pl>
2017-10-20 15:15:02 +0200

    kalkutator 1.0

--- program3.c
+++ program3.c
@@ -18,41 +18,37 @@ int odejmowanie(int a, int b){
 
 int main()
 {
-//	int a = 2, b = 5; /*deklaracja wraz z inicjalizacja */
+
 	int a;
 	int b;
 	char wybor;
-
+/*
 	printf("Podaj symbol dzialania matematycznego (+, -, *, /)\n");
-	scanf("%s", &wybor);	/*Odczytaj z konsoli i zapisz pod wybor*/
+	scanf("%c", &wybor);	/*Odczytaj z konsoli i zapisz pod wybor*/
 
 
 	printf("Podaj pierwsza liczbe\n");
 	scanf("%i", &a);	/*Odczytaj z konsoli i zapisz pod wybor*/
 
+
 	printf("Podaj druga liczbe\n");
 	scanf("%i", &b);	/*Odczytaj z konsoli i zapisz pod wybor*/
-
-	
-//	printf("Podaj symbol dzialania matematycznego (+, -, *, /)\n");
-//	scanf("%s", &wybor);	/*Odczytaj z konsoli i zapisz pod wybor*/
 	
 	
-
-	if(wybor == '+') dodaj(a, b);
-	else	if(wybor == '-') odejmowanie(a, b);
-	else	if(wybor == '*') mnoz(a, b);
-	else	if(wybor == '/') dziel(a, b);
+	printf("Podaj symbol dzialania matematycznego (+, -, *, /)\n");
+	scanf(" %c", &wybor);	/*Odczytaj z konsoli i zapisz pod wybor*/
+	
+/*	
+	printf("debug %i\n", a);
+	printf("debug %i\n", b);
+	printf("debug %c\n", wybor);
+*/
+	if(wybor == '+') dodaj(a,b);
+	else	if(wybor == '-') odejmowanie(a,b);
+	else	if(wybor == '*') mnoz(a,b);
+	else	if(wybor == '/') dziel(a,b);
 	else	printf("Dzialanie nie obslugiwane\n");
 
-/*
-	if(wybor == '*'){
-		printf("Wynik mnozenia: %i\n", mnoz(a,b));
-	}else{
-		printf("Wynik dodawania: %i\n", dodaj(a,b));
-	}
-
-*/
 
 	return 0;
 }
